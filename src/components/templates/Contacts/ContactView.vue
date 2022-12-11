d7addeb2b6014244137f29a63991af8f24e45acf
isaelmartinn <isael.martin.dev@gmail.com>
2022-12-11 14:22:49 -0600

    :sparkles: (ContactDelete): Show contact info
    Show contact info, show btn to delete contact and confirmation box to delete contact

--- src/components/templates/Contacts/ContactView.vue
+++ src/components/templates/Contacts/ContactView.vue
@@ -1,23 +1,50 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import type { Contact } from "@domain/models/Contact";
+import { ElMessage, ElMessageBox } from "element-plus";
 
 import ContactCard from "@organisms/Contacts/ContactCard.vue";
+
+type BtnOptions = {
+  isLoading: boolean;
+};
 
 interface Props {
   contact: Contact;
   loading?: boolean;
+  actions?: boolean;
+  btnOptions?: BtnOptions;
 }
 
 const props = defineProps<Props>();
 
 const emit = defineEmits<{
   (e: "on-click-go-back"): void;
+  (e: "on-click-delete-contact", id: string): void;
 }>();
 
 const contactFullName = computed(() => {
   return `${props.contact.name.first} ${props.contact.name.last}`;
 });
+
+const openConfirmationDialog = () => {
+  ElMessageBox.confirm(
+    `The next contact ${contactFullName.value} will be deleted. Continue?`,
+    "Delete contact",
+    {
+      confirmButtonText: "OK",
+      cancelButtonText: "Cancel",
+      type: "warning",
+    }
+  )
+    .then(() => emit("on-click-delete-contact", props.contact.id))
+    .catch(() => {
+      ElMessage({
+        type: "info",
+        message: "Delete canceled",
+      });
+    });
+};
 </script>
 
 <template>
@@ -35,6 +62,17 @@ const contactFullName = computed(() => {
       :phone="props.contact.phone"
       :created-at="props.contact.createdAt"
     ></contact-card>
+
+    <section v-if="props.actions" class="contactView__actions">
+      <el-button
+        :loading="props.btnOptions?.isLoading"
+        size="large"
+        type="danger"
+        @click="openConfirmationDialog"
+      >
+        Delete
+      </el-button>
+    </section>
   </main>
 </template>
 
@@ -42,14 +80,22 @@ const contactFullName = computed(() => {
 @use "sass:map";
 @use "@sass/fonts" as fonts;
 
+.contactView,
+.contactView__actions {
+  display: grid;
+}
+
 .contactView {
   width: min(100%, 800px);
   margin: 50px auto 0 auto;
-  display: grid;
   row-gap: 30px;
 }
 
 .contactView__title {
   font: map.get(fonts.$paragraph, "medium");
+}
+
+.contactView__actions {
+  justify-content: end;
 }
 </style>
--- src/views/Contacts/ContactDelete.vue
+++ src/views/Contacts/ContactDelete.vue
@@ -1,3 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import type { Contact } from "@domain/models/Contact";
+import { contactService } from "@domain/services/Contact.service";
 
-<template><main></main></template>
+import ContactView from "@templates/Contacts/ContactView.vue";
+
+const router = useRouter();
+const route = useRoute();
+
+const contactId = ref<string | string[]>("");
+
+let isLoading = ref<boolean>(false);
+let contact = ref<Contact>({
+  id: "",
+  name: {
+    first: "",
+    last: "",
+  },
+  email: "",
+  phone: "",
+  createdAt: "",
+});
+
+onMounted(async () => {
+  isLoading.value = true;
+  contactId.value = route.params.id;
+
+  const response = await contactService.getContact(contactId.value);
+  contact.value = response;
+
+  isLoading.value = false;
+});
+
+const handleGoBack = () => {
+  router.push({ name: "home" });
+};
+
+const handleDeleteContact = () => {};
+</script>
+
+<template>
+  <contact-view
+    actions
+    :contact="contact"
+    :loading="isLoading"
+    @on-click-go-back="handleGoBack"
+    @on-click-delete-contact="handleDeleteContact"
+  ></contact-view>
+</template>
