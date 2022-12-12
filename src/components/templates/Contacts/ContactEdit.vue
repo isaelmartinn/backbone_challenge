383f4962e36de1dfd5e6f152016e05a73d801b1b
isaelmartinn <isael.martin.dev@gmail.com>
2022-12-12 12:16:44 -0600

    :sparkles: (ContactEdit): Add goback btn and header

--- src/components/templates/Contacts/ContactEdit.vue
+++ src/components/templates/Contacts/ContactEdit.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "on-click-go-back"): void;
+}>();
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <el-page-header @back="emit('on-click-go-back')">
+      <template #content>
+        <h1 class="contactEdit__title">Edit contact</h1>
+      </template>
+    </el-page-header>
+  </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "sass:map";
+@use "@sass/fonts" as fonts;
+
+.contactEdit__title {
+  font: map.get(fonts.$paragraph, "medium");
+}
+</style>
--- src/views/Contacts/ContactEdit.vue
+++ src/views/Contacts/ContactEdit.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+
 import ContactEdit from "@templates/Contacts/ContactEdit.vue";
+
+const router = useRouter();
+
+const handleGoBack = () => {
+  router.push({ name: "home" });
+};
 </script>
 
 <template>
-  <contact-edit></contact-edit>
+  <contact-edit @on-click-go-back="handleGoBack"></contact-edit>
 </template>
